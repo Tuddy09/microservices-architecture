9707d555ad5748e84f116e88618964604dd5d64a
Tuddy09 <115088565+Tuddy09@users.noreply.github.com>
2024-08-01 14:34:54 +0300

    Swaped MySQL with redis for the Weather-Service.

--- weather_service/weather_module.py
+++ weather_service/weather_module.py
@@ -1,19 +1,11 @@
+import json
+
+import redis
+from decouple import Config
 from flask import request
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import *
-
-from microskel.db_module import Base
 
 
-class Weather(Base):
-    __tablename__ = 'weather'
-    id = Column(Integer, primary_key=True)
-    city = Column(String(128))
-    date = Column(Date)
-    temperature = Column(Float)
-    humidity = Column(Integer)
-    description = Column(String(128))
-
+class Weather:
     def __init__(self, id, city, date, temperature, humidity, description):
         self.id = id
         self.city = city
@@ -26,65 +18,65 @@ class Weather(Base):
         d = {}
         for k in self.__dict__.keys():
             if '_state' not in k:
-                d[k] = self.__dict__[k] if k != 'date' else self.__dict__[k].strftime('%Y-%m-%d')
+                d[k] = self.__dict__[k]
         return d
 
 
 def configure_views(app):
     @app.route('/weather', methods=['GET'])
-    def get_weather(db: SQLAlchemy):
-        try:
-            city = request.args.get('city')
-            date = request.args.get('date')
-            if city and date:
-                data = db.session.query(Weather).filter_by(city=city, date=date).all()
-                return [d.to_dict() for d in data]
-            else:
-                data = db.session.query(Weather).all()
-                return [d.to_dict() for d in data]
-        except Exception as e:
-            return str(e), 500
+    def get_weather():
+        city = request.args.get('city')
+        date = request.args.get('date')
+        if not city and not date:
+            weather = []
+            for key in client.keys():
+                weather.append(json.loads(client.get(key)))
+            return weather, 200
+        else:
+            for key in client.keys():
+                weather = json.loads(client.get(key))
+                if weather['city'] == city and weather['date'] == date:
+                    return [weather], 200
 
     @app.route('/weather', methods=['POST'])
-    def create_weather(db: SQLAlchemy):
-        try:
-            data = request.get_json()
-            id = data.get('id')
-            city = data.get('city')
-            date = data.get('date')
-            temperature = data.get('temperature')
-            humidity = data.get('humidity')
-            description = data.get('description')
-            w = Weather(id=id, city=city, date=date, temperature=temperature, humidity=humidity,
-                        description=description)
-            db.session.add(w)
-            db.session.commit()
-            return 'OK', 201
-        except Exception as e:
-            return str(e), 500
+    def create_weather():
+        data = request.get_json()
+        id = data.get('id')
+        city = data.get('city')
+        date = data.get('date')
+        temperature = data.get('temperature')
+        humidity = data.get('humidity')
+        description = data.get('description')
+        weather = Weather(id, city, date, temperature, humidity, description)
+        client.set(id, json.dumps(weather.to_dict()))
+        return 'OK', 201
 
     @app.route('/weather', methods=['PUT'])
-    def update_weather(db: SQLAlchemy):
-        try:
-            weather_id = request.args.get('id')
-            e = db.session.query(Weather).filter_by(id=weather_id).first()
-            data = request.get_json()
-            e.city = data.get('city')
-            e.date = data.get('date')
-            e.title = data.get('title')
-            e.description = data.get('description')
-            db.session.commit()
-            return 'OK', 200
-        except Exception as e:
-            return str(e), 500
+    def update_weather():
+        data = request.get_json()
+        id = request.args.get('id')
+        city = data.get('city')
+        date = data.get('date')
+        temperature = data.get('temperature')
+        humidity = data.get('humidity')
+        description = data.get('description')
+        weather = Weather(id, city, date, temperature, humidity, description)
+        client.set(id, json.dumps(weather.to_dict()))
+        return 'OK', 200
 
     @app.route('/weather', methods=['DELETE'])
-    def delete_weather(db: SQLAlchemy):
-        try:
-            weather_id = request.args.get('id')
-            w = db.session.query(Weather).filter_by(id=weather_id).first()
-            db.session.delete(w)
-            db.session.commit()
-            return 'OK', 200
-        except Exception as e:
-            return str(e), 500
+    def delete_weather():
+        id = request.args.get('id')
+        client.delete(id)
+        return 'OK', 200
+
+
+client = redis.Redis(host='redis-db')
+
+weathers = {
+    "1": Weather(1, 'Brasov', '2021-07-01', 25, 50, 'Sunny'),
+    "2": Weather(2, 'Bucuresti', '2021-07-01', 30, 60, 'Rainy')
+}
+
+for key, weather in weathers.items():
+    client.set(key, json.dumps(weather.to_dict()))
